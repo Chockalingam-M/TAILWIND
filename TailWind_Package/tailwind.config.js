c039f9c124fc1ba9abe54a5cdedfea2a08fa9116
Lingam_9914 <139003896+Chockalingam-M@users.noreply.github.com>
2024-09-16 15:41:28 +0530

    Add files via upload

---
+++ TailWind_Package/tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {},
+//     extend: {
+//     fontFamily: {
+//       lato: ['Lato','sans-serif'],
+//     },
+// },
+//   plugins: [],
+// }
+
+/** @type {import('../Tailwind_CSS/Tailwind_package/node_modules/tailwindcss/types').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
